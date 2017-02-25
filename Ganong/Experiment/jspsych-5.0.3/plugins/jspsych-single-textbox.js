d34a8be6198a0a340a80d14560873bcfc728218f
Ariel Cohen-Goldberg <ariel.goldberg@tufts.edu>
2017-02-25 16:47:13 -0500

    Add single-textbox plugin
    Added a plugin built on code by Peter Hilton to create a trial that
    displays text and a single textbox to input the answer (e.g., to get
    the SubjectID).

---
+++ Ganong/Experiment/jspsych-5.0.3/plugins/jspsych-single-textbox.js
@@ -0,0 +1,89 @@
+/**
+ * jspsych-single-textbox
+ *
+ * built on jspsych_survey_text and custom code from peter.g.hilton@gmail.com
+ *
+ */
+
+
+jsPsych.plugins['single-text'] = (function() {
+
+  var plugin = {};
+
+  plugin.trial = function(display_element, trial) {
+
+    trial.preamble = typeof trial.preamble == 'undefined' ? "" : trial.preamble;
+    if (typeof trial.size == 'undefined') {
+      trial.size = [];
+      for (var i = 0; i < trial.questions.length; i++) {
+        trial.size.push(30);
+      }
+    }
+
+    // if any trial variables are functions
+    // this evaluates the function and replaces
+    // it with the output of the function
+    trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial);
+
+    // show preamble text
+    display_element.append($('<div>', {
+      "id": 'jspsych-survey-text-preamble',
+      "class": 'jspsych-survey-text-preamble'
+    }));
+
+    $('#jspsych-survey-text-preamble').html(trial.preamble);
+
+    // add questions
+    for (var i = 0; i < trial.questions.length; i++) {
+      // create div
+      display_element.append($('<div>', {
+        "id": 'jspsych-survey-text-' + i,
+        "class": 'jspsych-survey-text-question'
+      }));
+
+      // add question text
+      $("#jspsych-survey-text-" + i).append('<p class="jspsych-survey-text">' + trial.questions[i] + '</p>');
+
+      // add text box
+      $("#jspsych-survey-text-" + i).append('<input type="text" class="response" id="jspsych-survey-text-response-' + i + '" size="' + trial.size[i] + '" autocomplete="off"/>');
+    }
+
+    // add submit button
+    display_element.append($('<button>', {
+      'id': 'jspsych-survey-text-next',
+      'class': 'jspsych-btn jspsych-survey-text'
+    }));
+    $("#jspsych-survey-text-next").html('Submit Answers');
+    $("#jspsych-survey-text-next").click(function() {
+      // measure response time
+      var endTime = (new Date()).getTime();
+      var response_time = endTime - startTime;
+
+      // create object to hold responses
+      var question_data;
+      $("div.jspsych-survey-text-question").each(function(index) {
+        var id = "Q" + index;
+        var val = $(this).children('.response').val();
+        var obje = {};
+        obje[id] = val;
+        question_data = parseInt(val)
+      });
+      
+
+      // save data
+      var trialdata = {
+        "rt": response_time,
+        "responses": JSON.stringify(question_data)
+      };
+
+      display_element.html('');
+
+      // next trial
+      jsPsych.finishTrial(trialdata);
+    });
+
+    var startTime = (new Date()).getTime();
+  };
+
+  return plugin;
+})();
